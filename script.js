a8b15bef5d89454689757aa8b41ae0956c620112
Adnan Ahmad <98473885+Adnanahmad27@users.noreply.github.com>
2024-02-13 05:57:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function makeid(l) {
   // write your code here
+	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	let res = "";
+	for(let i=0 ; i<l ; i++){
+		res += characters.charAt(Math.floor(Math.random()*characters.length));
+	}
+	return res;
 }
 
 // Do not change the code below.
